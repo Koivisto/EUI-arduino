e591dd1fbb8896718187ac9560774269b529ee76
Koivisto <aarne.j.leinonen@gmail.com>
2017-05-04 10:56:37 +0300

    Merge some git conflicts maybe

--- desktop-module/desktop-module.ino
+++ desktop-module/desktop-module.ino
@@ -28,9 +28,6 @@ String chairUpBad = "ChairUp :(";
 String warnedProsture = "";
 String sittingTooLongString = "Too long!";
 boolean isSittingTooLong = false;
-long currentMs, previousMs = 0L;
-long alertIntervalMs = 10000L;
-
 void setup() {
   serialSetup();
   LCDSetup();
