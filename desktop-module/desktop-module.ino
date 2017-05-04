a115e36f117dc767ab2983e6cb7c073458421ec9
Koivisto <aarne.j.leinonen@gmail.com>
2017-05-04 09:58:05 +0300

    Clean code

--- desktop-module/desktop-module.ino
+++ desktop-module/desktop-module.ino
@@ -4,10 +4,8 @@
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 int PIEZO_BUFFER_PIN = 9;
-
 int ONE_SECOND = 1000;
 double pauseBetweenNotesConstant = 1.3;
-
 int SYSTEM_STARTED_MELODY[] = {
   NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
 };
@@ -15,25 +13,20 @@ int SYSTEM_STARTED_MELODY[] = {
 int SYSTEM_STARTED_NOTE_DURATIONS[] = {
   4, 8, 8, 4, 4, 4, 4, 4
 };
-
 int ALERT_MELODY[] = {
   NOTE_C4, NOTE_C4, NOTE_C4
 };
 int ALERT_NOTE_DURATIONS[] = {
   8, 8, 8
 };
-
 String sitMapRow1 = " |  ";
 String sitMapRow2 = "- - ";
 String sitMapRow3 = " |  ";
 String sitMapRow4 = "    ";
-
 String chairUpGood = "ChairUp :)";
 String chairUpBad = "ChairUp :(";
-
 String warnedProsture = "";
 String sittingTooLongString = "Too long!";
-
 boolean isSittingTooLong = false;
 void setup() {
   serialSetup();
@@ -63,51 +56,6 @@ void LCDSetup() {
   // set up the LCD's number of columns and rows:
   lcd.begin(20, 4);  
 }
-
-/*
-//prints " " to 5 columns x 4 rows from startColumn onward
-void clear5Columns(Int startColumn) {
-  lcd.setCursor(startColumn, 0);
-  lcd.print("     ");
-  lcd.setCursor(startColumn, 1);
-  lcd.print("     ");
-  lcd.setCursor(startColumn, 2);
-  lcd.print("     ");
-  lcd.setCursor(startColumn, 3);
-  lcd.print("     ");
-}*7
-
-/*
-Left columns 15 to 20, 4 rows
-*/
-/*
-void printTo5Columns(Char charArray, Int startColumn) {
-  clear5Columns(startColumn);
-  int maxChars = 20;
-  int maxInRowRow = 20/4;
-  int len = sizeof(charArray);
-  for (i = 0; i < len %% i >= maxChars; i++){
-    if (i = 0){
-      lcd.setCursor(startColumn, 0);
-    }
-    else if (i = maxInRowRow + 1){
-      lcd.setCursor(startColumn, 1);
-    }
-    else if (i = maxInRowRow * 2 + 1){
-      lcd.setCursor(startColumn, 2);
-    }
-    else if (i = maxInRowRow * 3 + 1){
-      lcd.setCursor(startColumn, 3);
-    }
-    lcd.print(charArray[i]);
-    //Do I need to setCursor to next position?
-  }
-}
-
-void printHelloWorld(){
-  char *strToChar = "HelloWorld";
-  printTo5Columns(strToChar, 15);
-}*/
 
 void playSystemStaredMelody() {
   for (int thisNote = 0; thisNote < 8; thisNote++) {
