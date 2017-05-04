5bd4997b7e3bae105a745e5cbdf4ef19a4188ab5
Koivisto <aarne.j.leinonen@gmail.com>
2017-05-04 10:59:08 +0300

    I have no Ide what my git does
    This reverts commit cd5104cca8703e0fcf905c2e5cd457e818074934.
    
    # Conflicts:
    #	desktop-module/desktop-module.ino

--- desktop-module/desktop-module.ino
+++ desktop-module/desktop-module.ino
@@ -41,11 +41,16 @@ void setup() {
 }
 
 void loop() {
+<<<<<<< HEAD
   currentMs = millis();
   // if person has been sitting for too long and alertIntervalMs has passed, play new alert
   if (isSittingTooLong && (currentMs - previousMs > alertIntervalMs)) {
     playAlertMelody(2);
     previousMs = millis()
+=======
+  if (isSittingTooLong) {
+    playAlertMelody();
+>>>>>>> parent of cd5104c... Set 10000 ms delay to alert playing
     isSittingTooLong = false;
   }
   delay(1000);
